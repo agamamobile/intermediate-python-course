107f552b498555b3ecd6efaa30544397cfac59e2
agamamobile <53582220+agamamobile@users.noreply.github.com>
2020-12-23 02:55:41 -0500

    Input

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,16 @@
 import random
-dice_rolls = 2
 def main():
   dice_sum = 0
+  dice_rolls = int(input('how many dice to roll?'))
+  dice_sides = int(input('how many sides?'))
   for i in range(0, dice_rolls):
-    die = random.randint(1, 6)
-    print(f'You rolled a {die}')
+    die = random.randint(1, dice_sides)
+    if die == 1:
+      print(f'You rolled a {die}. Failed')
+    elif die == dice_sides:
+      print(f'You rolled a {die}. Success')
+    else:
+      print(f'You rolled a {die}')
     dice_sum += die
   print(f'You rolled total {dice_sum}')
 if __name__== "__main__":
