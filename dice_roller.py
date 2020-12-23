bbdd6bcab2c6c3a584c0ca2d452a962a5474bec4
agamamobile <53582220+agamamobile@users.noreply.github.com>
2020-12-23 02:43:47 -0500

    sum of dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
+import random
+dice_rolls = 2
 def main():
-  print('You rolled a die')
-
+  dice_sum = 0
+  for i in range(0, dice_rolls):
+    die = random.randint(1, 6)
+    print(f'You rolled a {die}')
+    dice_sum += die
+  print(f'You rolled total {dice_sum}')
 if __name__== "__main__":
   main()
